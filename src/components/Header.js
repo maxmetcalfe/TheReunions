84065f299c3dd0ca8fda3c14d900396598cf48dd
Max Metcalfe <m.maxmetcalfe@gmail.com>
2018-01-13 11:47:05 -0800

    Adding new Header component.

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+
+  render() {
+      return (
+        <div>
+          <h1>Welcome to The Reunions.</h1>
+          <h2>
+            A group of four friends travel together around the world.
+            This is where they have been.
+          </h2>
+        </div>
+      );
+  }
+}
+
+export default Header;
