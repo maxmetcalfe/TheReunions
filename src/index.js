49873811b2316ade889ad70ec2ef8600cb2f9eeb
Max Metcalfe <m.maxmetcalfe@gmail.com>
2018-01-13 16:59:39 -0800

    Load map when MapContainer mounts.

--- src/index.js
+++ src/index.js
@@ -9,5 +9,3 @@ require("./main.css");
 import App from "./components/App";
 
 ReactDOM.render(<App summaryCounts={countdata.summaryCounts} reunions={reunionData.reunions} reunionsForMember={reunionData.reunionsForMember}/>, document.getElementById("app"));
-
-require("./map.js");
