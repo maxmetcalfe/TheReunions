0dbd917f8f673932a9919db1ed029fd8fa3e2731
Max Metcalfe <m.maxmetcalfe@gmail.com>
2018-01-25 21:16:18 -0800

    Improve ReunionList.

--- src/components/ReunionList.js
+++ src/components/ReunionList.js
@@ -2,25 +2,13 @@ import React, { Component } from "react";
 
 class ReunionList extends Component {
 
-  componentDidMount() {
-    // Scroll the div to the bottom on mount.
-    this.refs.element.scrollTop = this.refs.element.scrollHeight;
-  }
-
-  createStyle(index) {
-    return {
-      fontSize: 50 - index
-    };
-  }
-
   render() {
     return (
       <div id="reunion-list" ref="element">
         <table>
           <tbody>
             {this.props.reunions.features.reverse().map((reunion, index) => (
-
-              <tr className={reunion.properties.category} style={this.createStyle(index)}key={reunion.properties.element_id}>
+              <tr className={reunion.properties.category}>
                 <td>{reunion.properties.location} - {reunion.properties.name}</td>
               </tr>
             ))}
