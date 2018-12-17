6a9561ed32f7f589465b2b1d0e887e41d9a00a30
Max Metcalfe <m.maxmetcalfe@gmail.com>
2018-12-16 19:12:00 -0800

    Clean up server.js.

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express = require("express");
-const app = express();
-const path = require("path");
+const express = require("express")
+const app = express()
+const path = require("path")
 
+const port = process.env.PORT || 8080
 
-app.use(express.static(path.join(__dirname + "/dist")));
+app.use(express.static(path.join(__dirname + "/dist")))
 
-app.get("/",function(req,res){
+app.get("/",function(req, res) {
    res.sendFile(path.join(__dirname + "dist/index.html"))
 })
 
-app.set('port', (process.env.PORT || 3000));
+app.set("port", (port))
+
 app.listen(app.get("port"), function () {
-  console.log("Running on 3000!")
+  console.log( `Running on ${port}!`)
 })
