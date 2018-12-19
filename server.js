15e69fe3111e6c3b5df16737b658709792a69473
Max Metcalfe <m.maxmetcalfe@gmail.com>
2018-12-18 21:14:31 -0800

    Add data route to fetch data from Google Sheets.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const app = express()
 const path = require("path")
+const data = require("./src/routes/data")
 
 const port = process.env.PORT || 8080
 
 app.use(express.static(path.join(__dirname + "/dist")))
+
+app.use("/data", data)
 
 app.get("/",function(req, res) {
    res.sendFile(path.join(__dirname + "dist/index.html"))
---
+++ src/routes/data.js
@@ -0,0 +1,61 @@
+const express = require("express")
+const router = express.Router()
+const GoogleSpreadsheet = require("google-spreadsheet")
+const async = require("async")
+
+const fields = [
+  "name",
+  "location",
+  "lat",
+  "lng",
+  "category",
+  "members"
+]
+
+const googleSheet = new GoogleSpreadsheet(process.env.SHEET_ID)
+
+function checkWorksheetTitle(worksheet) {
+  return worksheet.title === process.env.WORKSHEET_TITLE
+}
+
+let sheet
+let reunions = []
+
+router.use("/", function(req, res) {
+  async.series([
+    function getSheet(step) {
+      googleSheet.getInfo(function(err, info) {
+        sheet = info.worksheets.filter(checkWorksheetTitle)[0]
+        if (!sheet) {
+          res.sendStatus(404)
+          return
+        }
+        step()
+      })
+    },
+
+    function readWorksheet(step) {
+      sheet.getRows({
+        offset: 2,
+        "limit": 100
+      }, function(err, rows) {
+        rows.forEach(function(row) {
+          var reunion = {}
+          fields.forEach(function(field) {
+            reunion[field] = row[field]
+          })
+          reunions.push(reunion)
+        })
+
+        res.json(reunions)
+        return
+        step()
+      })
+    }
+  ], function (err, result) {
+    res.sendStatus(err)
+  })
+})
+
+
+module.exports = router
