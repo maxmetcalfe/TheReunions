3114472a074e6ea58687689273575c17fd094d7e
Max Metcalfe <m.maxmetcalfe@gmail.com>
2019-02-26 18:21:05 -0800

    Updating reunion data.

--- server/data/countData.js
+++ server/data/countData.js
@@ -1,8 +1,8 @@
 var summaryCounts = {
-    "j":{'four': 13, 'three': 4, 'two': 14},
-    "m":{'four': 13, 'three': 5, 'two': 32},
-    "d":{'four': 13, 'three': 3, 'two': 11},
-    "g":{'four': 12, 'three': 3, 'two': 25},
+    "j":{'four': 14, 'three': 4, 'two': 14},
+    "m":{'four': 14, 'three': 5, 'two': 32},
+    "d":{'four': 14, 'three': 3, 'two': 11},
+    "g":{'four': 13, 'three': 3, 'two': 25},
 };
 
 exports.summaryCounts = summaryCounts;
