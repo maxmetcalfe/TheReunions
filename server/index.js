9a4c80842e1d3cabf53355948a9d48563e4299c7
Max Metcalfe <hello@maxmetcalfe.com>
2019-12-02 22:33:50 -0600

    Start receiving text messages.

--- server/index.js
+++ server/index.js
@@ -2,18 +2,20 @@ const express = require("express")
 const app = express()
 const path = require("path")
 const data = require("./routes/data")
+const bodyParser = require("body-parser")
 const sms = require("./routes/sms")
-
 
 const port = process.env.PORT || 8080
 
 app.use(express.static(path.join(__dirname, "..", "/client/dist")))
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/data", data)
 
 app.use("/sms", sms)
 
-app.get("/",function(req, res) {
+app.get("/", function(req, res) {
    res.sendFile(path.join(__dirname, "..", "/client/dist/index.html"))
 })
 
--- server/routes/sms.js
+++ server/routes/sms.js
@@ -1,27 +1,39 @@
 const express = require("express")
 const router = express.Router()
-const GoogleSpreadsheet = require("google-spreadsheet")
 const async = require("async")
+const GoogleSpreadsheet = require("google-spreadsheet")
+const MessagingResponse = require("twilio").twiml.MessagingResponse;
+// SMS Debug. Remove committing. //
+const accountSid = 'AC31802eb19c418e6df06c7a80ea85e540';
+const authToken = '4c066c996d3fa741bd48807c1545a97b';
+const client = require('twilio')(accountSid, authToken);
+///////////////////////////////////////////////////////
 
-const fields = [
-  "name",
-  "location",
-  "lat",
-  "lng",
-  "category",
-  "members"
-]
+router.post("/", function(req, res) {
+  console.log(req.body.Body);
 
-const googleSheet = new GoogleSpreadsheet(process.env.SHEET_ID)
+  async.series([
+    function getSheet(step) {
+      googleSheet.getInfo(function(err, info) {
+        sheet = info.worksheets.filter(checkWorksheetTitle)[0]
+        console.log(sheet);
+        if (!sheet) {
+          res.sendStatus(404)
+          return
+        }
+        step()
+      })
+    },
+  ], function (err) {
+    res.sendStatus(err)
+  })
+  // const twiml = new MessagingResponse();
+  // 
+  // twiml.message("Reunions info received. \n\nSee: thereunions.com");
+  // 
+  // res.writeHead(200, {'Content-Type': 'text/xml'});
+  // res.end(twiml.toString());
+})
 
-function checkWorksheetTitle(worksheet) {
-  return worksheet.title === process.env.WORKSHEET_TITLE
-}
-
-let sheet
-
-router.get("/sms", function(req, res) {
-  res.send("Hello world!");
-});
 
 module.exports = router
