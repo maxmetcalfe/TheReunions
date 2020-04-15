f1d787c137ece8375f746a513695de462e97a750
Max Metcalfe <hello@maxmetcalfe.com>
2020-04-15 08:33:54 -0700

    Remove unused data route

--- server/index.js
+++ server/index.js
@@ -5,13 +5,10 @@ const middleware = require("webpack-dev-middleware");
 const compiler = webpack(require("../webpack.config.js"));
 
 const app = express()
-const data = require("./routes/data")
 
 const port = process.env.PORT || 8080
 
 app.use(express.static(path.join(__dirname, "..", "/client/dist")))
-
-app.use("/data", data)
 
 app.use(middleware(compiler));
 
