c293377ba0669f73aa2de5f4b2d92b32ea122a40
Max Metcalfe <hello@maxmetcalfe.com>
2021-03-12 16:42:08 -0800

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,33 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
 
 module.exports = {
   entry: [
-    './client/index'
+    "./client/index"
   ],
+  mode: "production",
   module: {
     rules: [
-      { test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css$/, loader: "style-loader!css-loader" }
+      { test: /\.js?$/, use: "babel-loader", exclude: /node_modules/ },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] }
     ]
   },
   resolve: {
     modules: [
-      path.resolve('./'),
-      path.resolve('./node_modules')
+      path.resolve("./"),
+      path.resolve("./node_modules")
     ],
-    extensions: ['.js']
+    extensions: [".js"]
   },
   output: {
-    path: path.join(__dirname, '/client/dist'),
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: path.join(__dirname, "/client/dist"),
+    publicPath: "/",
+    filename: "bundle.js"
   },
   devServer: {
     contentBase: path.join(__dirname, "/client/dist"),
     hot: true,
     port: 8080
   },
-  plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
-  ],
   devtool: "source-map"
 };
